dc7fcc7f69ac8fbc27c92a5f41e66d7a82747c82
ivanbrennan <ivan.brennan@gmail.com>
2018-08-02 00:04:49 -0400

    warn about node_modules directory

--- shell.nix
+++ shell.nix
@@ -9,6 +9,15 @@ in
 
 helloVue.shell.overrideAttrs (oldAttrs: rec {
   shellHook = prompt + oldAttrs.shellHook + ''
-    ln -svnf $NODE_PATH node_modules
+    if [[ -s node_modules ]] && [[ ! -L node_modules ]]; then
+        printf "
+    %s%sWARNING%s: There is a node_modules/ folder in the root directory of the project!
+    This can interfer with nix-shell's ability to find node command line utilities
+    and will shadow the node packages installed by Nix.
+    If you don't want this to happen, then you should remove it and restart nix-shell.
+    " $(tput bold) $(tput setaf 1) $(tput sgr0)
+    else
+        ln -svnf $NODE_PATH node_modules
+    fi
   '';
 })
